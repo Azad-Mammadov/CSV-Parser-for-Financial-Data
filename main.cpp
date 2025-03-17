da5ee380f88a4e52b933eb085b2344531ba3052e
Azad Mammadov <45455149+Azad-Mammadov@users.noreply.github.com>
2025-03-18 00:17:41 +0100

    new update

--- main.cpp
+++ main.cpp
@@ -1,15 +1,30 @@
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 
 int main()
 {
-    std::ifstream file("input.txt");
+    std::ifstream file("C:\\Users\\AzadMammadov\\Documents\\GitHub\\CSV Parser for Financial Data\\CSV_Files\\input.csv");
+    if (!file.is_open())
+    {
+        std::cerr << "Error opening file" << std::endl;
+        return 1;
+    }
+
     std::string line;
     while (std::getline(file, line))
     {
-        std::cout << line << std::endl;
+        std::stringstream ss(line);
+        std::string field;
+        while (std::getline(ss, field, ','))
+        {
+            std::cout << field <<" ";
+        }
+        std::cout << std::endl;
     }
+
+    file.close(); // for closing opened file
     return 0;
 
 
